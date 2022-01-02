66ee9032354f58437365ffd38046c2383238c4aa
Christian Johnston <cjjohnston05@gmail.com>
2022-01-01 23:02:01 -0800

    RNG Abomination
    Works eventually... but rng is definitely not the best way to do this

--- microMouseServer/studentai.cpp
+++ microMouseServer/studentai.cpp
@@ -3,9 +3,36 @@
 
 void microMouseServer::studentAI()
 {
-moveForward() ;
+    //branches
+    if ((isWallLeft() + isWallRight() + isWallForward()) <= 1)
+    {
+        if (isWallLeft()) //  |_
+        {
+            int dir = rand() % 2 ;
+            if (dir==0) { turnRight(); }
+        } else if (isWallRight()) { // _|
+            int dir = rand() % 2 ;
+            if (dir==0) { turnLeft(); }
+        } else if (isWallForward()) { // =
+            int dir = rand() % 2 ;
+            if (dir==0) { turnLeft(); }
+            else { turnRight(); }
+        } else { // _|_
+            int dir = rand() % 3 ;
+            if(dir == 0) { turnRight(); }
+            else if (dir == 1) { turnLeft(); }
+        }
+    }
 
+    if(!isWallForward()) { moveForward(); }
+    else { turnRight(); }
+
+    /* Wall hugging:
+    if (!moveForward()) { turnRight() ; }
+    if(!isWallLeft()) { turnLeft() ; }
+    */
 }
+
 /*
  * The following are the eight functions that you can call. Feel free to create your own fuctions as well.
  * Remember that any solution that calls moveForward more than once per call of studentAI() will have points deducted.
